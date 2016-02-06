9ac51865158858a319261ad8c4677e848b99ea50
Jeremy Geros <jeremy453@gmail.com>
2016-02-06 08:20:42 +0700

    Rails 4.1 comp

--- lib/delayed_resque.rb
+++ lib/delayed_resque.rb
@@ -1,5 +1,5 @@
 require 'resque'
-require 'resque_scheduler'
+require 'resque/scheduler'
 require File.dirname(__FILE__) + '/delayed_resque/message_sending'
 require File.dirname(__FILE__) + '/delayed_resque/performable_method'
 
