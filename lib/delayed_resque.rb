f70db3e51e876966e2a231c9f9e032a0509dec66
Chris Waters <k1w1@users.noreply.github.com>
2020-06-16 11:33:29 -0700

    Revert "PF-163 resque unique job handling"

--- lib/delayed_resque.rb
+++ lib/delayed_resque.rb
@@ -1,6 +1,5 @@
 require 'resque'
 require 'resque-scheduler'
-require File.dirname(__FILE__) + '/delayed_resque/meta_data'
 require File.dirname(__FILE__) + '/delayed_resque/message_sending'
 require File.dirname(__FILE__) + '/delayed_resque/performable_method'
 
